1f167751ca52c15c7a97006d8ef7d74b88c3474e
Sidicley Schneider <sidi.fev@gmail.com>
2022-02-09 14:33:05 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <p>Project</p>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
